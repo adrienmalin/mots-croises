13fcdf05fbec4d3b9ff5dbde3ab3413aff3b5ab8
adrien <adrien@malingrey.fr>
2025-05-08 01:47:58 +0200

    grand dico

--- dico.php
+++ dico.php
@@ -14,6 +14,9 @@ function dico($longueur_max) {
     $transliterator = Transliterator::createFromRules(':: Any-Latin; :: Latin-ASCII; :: NFD; :: [:Nonspacing Mark:] Remove; :: Upper(); :: NFC;', Transliterator::FORWARD);
     
     $dico = [[""]];
+    for ($longueur = 0; $longueur <= $longueur_max; $longueur++) {
+        $dico[] = new Trie();
+    }
     if (($lecteur = fopen("dico.csv", "r")) !== FALSE) {
         $entete = fgetcsv($lecteur, 0, "\t");
         while (($ligne = fgetcsv($lecteur, 0, "\t")) !== FALSE) {
@@ -23,39 +26,26 @@ function dico($longueur_max) {
                 || strlen($ligne[0]) > $longueur_max
             ) continue;
 
-            switch(count($ligne)) {
-                case 1:
-                    [$mot] = $ligne;
-                    $definition = "";
-                break;
-                case 2:
-                    [$mot, $definition] = $ligne;
-                break;
-                case 3:
-                    [$mot, $definition, $auteur] = $ligne;
-                    $definition .= " <small><em>$auteur</em></small>";
-                break;
-            }
-
             $mot = $ligne[0];
             $definitions = array_slice($ligne, 1);
+            $mot = str_replace("-", " ", $mot);
             
             $mot = $transliterator->transliterate($mot);
             if (strpos($mot, " ") !== false) {
                 $mots = explode(" ", $mot);
-                $nb_mot = count($mots);
+                $nb_mots = count($mots);
                 $mot = implode("", $mots);
-                $definition .= " ($nb_mot mots)";
+                foreach($definitions as $i => $definition) {
+                    $definitions[$i] .= " ($nb_mots mots)";
+                }
             }
 
             $longueur = strlen($mot);
-            if (!isset($dico[$longueur])) $dico[$longueur] = new Trie();
-            if (!isset($dico[$longueur][$mot])) $dico[$longueur][$mot] = [];
-            if (strlen($definition)) $dico[$longueur][$mot][] = $definition;
+            $dico[$longueur][$mot] = $definitions;
         }
         fclose($lecteur);
     }
-
+    
     return $dico;
 }
 
